978b085e4f4fa39c16efc583fe0dd50904fdbbd6
Emircan <emircandv787@gmail.com>
2024-12-02 23:59:40 +0300

    The AESService interface and concrete class have been defined and implemented.

---
+++ EncrptDecrptAPI/Services/Implementations/AESService.cs
@@ -0,0 +1,44 @@
+﻿using EncrptDecrptAPI.Services.Interfaces;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace EncrptDecrptAPI.Services.Implementations
+{
+    public class AESService : IAESService
+    {
+        public string Encrypt(string key, string IV, string data)
+        {
+            using var aes = Aes.Create();
+
+            aes.IV = Encoding.UTF8.GetBytes(IV);
+            aes.Key = Encoding.UTF8.GetBytes(key);
+
+            using var encrptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            using var memoryStream = new MemoryStream();
+            using var cryptoStream = new CryptoStream(memoryStream, encrptor, CryptoStreamMode.Write);
+            using var writer = new StreamWriter(cryptoStream);
+
+            writer.Write(data);
+
+            return Convert.ToBase64String(memoryStream.ToArray());
+
+        }
+
+        public string Decrypt(string key, string IV, string data)
+        {
+
+            using var aes = Aes.Create();
+
+            aes.IV = Encoding.UTF8.GetBytes(IV);
+            aes.Key = Encoding.UTF8.GetBytes(key);
+
+            using var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            using var memoryStream = new MemoryStream(Convert.FromBase64String(data));
+            using var cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            using var reader = new StreamReader(cryptoStream);
+
+            return reader.ReadToEnd();
+
+        }
+    }
+}
---
+++ EncrptDecrptAPI/Services/Interfaces/AESService.cs
@@ -0,0 +1,9 @@
+﻿namespace EncrptDecrptAPI.Services.Interfaces
+{
+    public interface IAESService
+    {
+        string Encrypt(string key, string IV, string data);
+        string Decrypt(string key, string IV, string data);
+
+    }
+}
