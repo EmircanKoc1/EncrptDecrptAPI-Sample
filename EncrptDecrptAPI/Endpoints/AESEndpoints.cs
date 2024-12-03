05e87712ed1b5b05cfefaa06b190924aee57c6f7
Emircan <emircandv787@gmail.com>
2024-12-03 23:04:13 +0300

    The AESEndpoints have been defined and completed.

--- EncrptDecrptAPI/Endpoints/AESEndpoints.cs
+++ EncrptDecrptAPI/Endpoints/AESEndpoints.cs
@@ -1,4 +1,9 @@
-﻿namespace EncrptDecrptAPI.Endpoints
+﻿using EncrptDecrptAPI.Models.Response;
+using EncrptDecrptAPI.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace EncrptDecrptAPI.Endpoints
 {
     public static class AESEndpoints
     {
@@ -6,17 +11,36 @@
         public static void MapAESEndpoints(this WebApplication app)
         {
 
-            app.MapGet("encrpt", () =>
+            app.MapPost("encrpt", (
+                [FromServices] IAESService _aesService,
+                 [FromBody] AESEncryptRequestModel requestModel) =>
             {
+                var responseObject = _aesService.Encrypt(requestModel.Key, requestModel.IV, requestModel.data);
 
-            });
+                return responseObject.isSuccess
+                ? Results.Ok(responseObject)
+                : Results.BadRequest(responseObject);
 
 
-            app.MapGet("decrpt", () =>
+            }).WithTags("Aes Methods")
+              .Produces<AESEncryptResponseModel>((int)HttpStatusCode.OK)
+              .Produces<AESEncryptResponseModel>((int)HttpStatusCode.BadRequest);
+
+
+
+            app.MapPost("decrpt", (
+                  [FromServices] IAESService _aesService,
+                  [FromBody] AESDecryptRequestModel requestModel) =>
             {
+                AESDecryptResponseModel? responseObject = _aesService.Decrypt(requestModel.Key, requestModel.IV, requestModel.data);
 
-            });
+                return responseObject.isSuccess
+                ? Results.Ok(responseObject)
+                : Results.BadRequest(responseObject);
 
+            }).WithTags("Aes Methods")
+            .Produces<AESDecryptResponseModel>((int)HttpStatusCode.OK)
+            .Produces<AESDecryptResponseModel>((int)HttpStatusCode.BadRequest);
 
         }
 
