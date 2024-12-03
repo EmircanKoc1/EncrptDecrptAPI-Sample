bc73a247ce5ef8a255d16a86073286b5aa33f51b
Emircan <emircandv787@gmail.com>
2024-12-03 23:01:06 +0300

    The IAESService has been renamed and refactored.

---
+++ EncrptDecrptAPI/Services/Interfaces/IAESService.cs
@@ -0,0 +1,11 @@
+﻿using EncrptDecrptAPI.Models.Response;
+
+namespace EncrptDecrptAPI.Services.Interfaces
+{
+    public interface IAESService
+    {
+        AESEncryptResponseModel Encrypt(string? key, string? IV, string data);
+        AESDecryptResponseModel Decrypt(string key, string IV, string data);
+
+    }
+}
