58e91a8585539fae1aaf511d0d087244fba59f9b
Emircan <emircandv787@gmail.com>
2024-12-03 23:01:32 +0300

    The AESModel records have been defined.

---
+++ EncrptDecrptAPI/Models/AES/AESModels.cs
@@ -0,0 +1,9 @@
+﻿namespace EncrptDecrptAPI.Models.Response;
+
+public record AESEncryptResponseModel(string Key, string IV, string encryptedValue, bool isSuccess);
+
+public record AESEncryptRequestModel(string? Key, string? IV, string data);
+
+
+public record AESDecryptResponseModel(string Data, bool isSuccess);
+public record AESDecryptRequestModel(string Key, string IV, string data);
